cb801b710c3b443ccfeaa4539b4d9532136c79a7
JULIO-WIN10 <julio.silva.cwb@gmail.com>
2019-06-24 11:13:57 -0300

    ajustes imagem

--- resources/js/components/HomeComponent.js
+++ resources/js/components/HomeComponent.js
@@ -7,7 +7,7 @@ class HomeComponent {
             `<div class="home">
                 <div class="row">
                     <div class="col-md-8 offset-md-2 box-img">
-                        <img class="imagem" src="resources/img/image-home.svg">
+                        <img src="resources/img/image-home.svg" class="imagem figure-img img-fluid">
                     </div>
                 </div>
                 <form id="form">
@@ -17,10 +17,10 @@ class HomeComponent {
                                 Usuário não encontrado
                             </div>
                         </div>
-                        <div class="col-md-6 offset-md-2">
-                            <input class="form-control" id="search" type="search" placeholder="Informe o nome do usuário do gihub" aria-label="Pesquisar">
+                        <div class="col-md-6 offset-md-2 col-sm-12">
+                            <input class="form-control" id="search" type="search" placeholder="Informe o usuário do gihub" aria-label="Pesquisar">
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-2 col-sm-6" >
                             <button class="btn btn-outline-success" type="button" onclick="rotas.homeController.buscarUsuario()">Pesquisar</button>
                         </div>
                     </div>
